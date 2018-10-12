a0effcd1ea8e65959c4ff48bace9fd2b919fddab
Ntando <ntandofitshaneversace@gmail.com>
2018-10-12 16:03:14 +0200

    media query fixed

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.post('/clear', async function (req, res,) {
 });
 
 
-let PORT = process.env.PORT || 3007;
+let PORT = process.env.PORT || 3009;
 
 app.listen(PORT, function () {
     console.log('App running on port', PORT);
