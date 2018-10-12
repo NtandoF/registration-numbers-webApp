64e2ca6bb76d948286af890f8be15f46d5d4610d
Ntando <ntandofitshaneversace@gmail.com>
2018-10-12 12:38:11 +0200

    added media queries and rejex

--- regTable.sql
+++ regTable.sql
@@ -17,4 +17,5 @@ INSERT INTO towns (town_name, reg) VALUES ('GEORGE', 'CAW ');
 INSERT INTO towns (town_name, reg) VALUES ('PAARL', 'CJ ');
 INSERT INTO towns (town_name, reg) VALUES ('BELVILLE', 'CY '); 
 
+
  
